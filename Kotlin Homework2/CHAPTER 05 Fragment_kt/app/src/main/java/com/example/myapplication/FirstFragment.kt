6e65b834b46df35f02070641af8b9712a5dc22c2
ted <0966739639@gmail.com>
2021-11-10 22:40:25 +0800

    Kotlin Homework2

---
+++ Kotlin Homework2/CHAPTER 05 Fragment_kt/app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -0,0 +1,61 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("FirstFragment","onCreate")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        Log.e("FirstFragment","onCreateView")
+        return inflater.inflate(R.layout.fragment_first,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.e("FirstFragment","onActivityCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("FirstFragment","onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.e("FirstFragment","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("FirstFragment","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("FirstFragment","onStop")
+    }
+
+    fun onDestoryView() {
+        super.onDestroyView()
+        Log.e("FirstFragment","onDestoryView")
+    }
+
+    fun onDestory() {
+        super.onDestroy()
+        Log.e("FirstFragment","onDestory")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.e("FirstFragment","onDetach")
+    }
+}
